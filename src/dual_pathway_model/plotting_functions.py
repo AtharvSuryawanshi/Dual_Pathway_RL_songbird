13d261fb9bce5137275392753ddde342790a0c7e
atharv mpg iris <atharvssuryawanshi012@gmail.com>
2026-02-06 21:24:47 +0100

    fig2 landscape univ function

--- src/dual_pathway_model/plotting_functions.py
+++ src/dual_pathway_model/plotting_functions.py
@@ -4,6 +4,8 @@ import numpy as np
 import seaborn as sns
 from pathlib import Path
 import yaml
+from matplotlib.colors import LinearSegmentedColormap
+from dual_pathway_model.functions import *
 
 # Parameters 
 # Get path relative to this file
@@ -144,6 +146,79 @@ def plot_results_violin(returns, params,
 
     ax2.spines['top'].set_visible(False)
     ax2.spines['right'].set_visible(False)
-
-
     plt.show()
+
+
+# LANDSCAPE PLOT
+def plot_lansdcape_only(obj, syll, plot_colors = plot_colors):
+    fig, axs = plt.subplots(figsize=(9, 9))
+    cmap = LinearSegmentedColormap.from_list('change_this', ['white', 'white']) # 'Greys' color_contour_bckg #'Purples' #LinearSegmentedColormap.from_list('white_to_black', ['white', 'rebeccapurple'])
+    levels_ = 50
+    ##### Artificial Landscapes #####
+    def plot_artificial():
+        limit = obj.limit
+        x, y = np.linspace(-limit, limit, 50), np.linspace(-limit, limit, 50)
+        X, Y = np.meshgrid(x, y)
+        Z = obj.get_reward([X, Y], syll)
+        contour = axs.contourf(X, Y, Z, levels=levels_, cmap=cmap)
+        if if_contour:
+            axs.contour(X, Y, Z, levels=15, colors='k', linewidths=1, alpha=0.9)
+        # cbar = fig.colorbar(contour, ax=axs)
+        # cbar.set_label('Performance Metric (R)', fontsize=20, rotation = 270)
+        # cbar.ax.tick_params(labelsize=18)
+        # cbar.ax.set_yticks([0,1])
+
+        axs.set_xticks([-limit, 0, limit], [-1, 0, 1])
+        axs.set_yticks([-limit, 0, limit], [-1, 0, 1])
+    ##### Syrinx Landscapes #####
+    def plot_syrinx():
+        if obj.N_SYLL > 4:
+            raise ValueError('Only 4 syllables are available in the syrinx landscape')
+        obj.syrinx_contours = []
+        obj.syrinx_targets = []
+        for j in range(obj.N_SYLL):
+            base = np.load(f"contours/Syll{j+1}.npy")
+            Z, target = make_contour(base)
+            obj.syrinx_contours.append(Z)
+            obj.syrinx_targets.append(target)
+        obj.centers = np.array(obj.syrinx_targets)
+        obj.syrinx_contours = np.array(obj.syrinx_contours)
+        Z = obj.syrinx_contours[syll]
+        target_pos = obj.syrinx_targets[syll]
+        if if_contour:
+            axs.contour(Z.T, levels=15, extent=[-1, 1, -1, 1], colors='k', linewidths=1, alpha=1)
+        cs = axs.contourf(Z.T, cmap=cmap, extent=[-1, 1, -1, 1], levels=levels_)
+        # cbar = fig.colorbar(cs, ax = axs)
+        # cbar.set_label('Performance Metric (R)', fontsize=20, rotation = 270)
+        # cbar.ax.tick_params(labelsize=18)
+        # cbar.ax.set_yticks([0,1])
+        axs.set_xticks([-1, 1], [0, 1])
+        axs.set_yticks([-1,1], [0, 0.2])
+        # axs.scatter(target_pos[1], target_pos[0], s=100, c='green', marker='x', label='Target')
+    if obj.LANDSCAPE == 0:
+        print("Plotting artificial landscape")
+        plot_artificial()
+    else:
+        print("Plotting syrinx landscape")
+        plot_syrinx()
+    # if not force_landscape:
+    #     if obj.LANDSCAPE == 0:
+    #         print("No force artificial landscape")
+    #         plot_artificial()
+    #     else:
+    #         plot_syrinx()
+    # else:
+    #     if landscape == 0:
+    #         plot_artificial()
+    #     else:
+    #         plot_syrinx()
+
+    # axs.set_ylabel(r'$P_{\alpha}$', fontsize=22)
+    # axs.set_xlabel(r'$P_{\beta}$', fontsize=22)
+    axs.set_ylabel(r'$P$', fontsize=22)
+    axs.set_xlabel(r'$T$', fontsize=22)
+    axs.tick_params(labelsize=16)
+    axs.legend()
+    plt.tight_layout()
+    plt.show()
+
